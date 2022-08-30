f19fe97793214c2c751e736db2c53a29cee9167e
SKowalsky <skowalsky37@gmail.com>
2022-08-30 09:59:55 +0200

    1 file modified

--- pages/kowalsky.js
+++ pages/kowalsky.js
@@ -7,12 +7,12 @@ export default function Kowalsky() {
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome Kowalsky! Test.
-        </h1>
+        <h1 className={styles.title}>Welcome Kowalsky! Test.</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
