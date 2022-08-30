bff09a36fbcf33581425a85789affa4ca5f63565
SKowalsky <skowalsky37@gmail.com>
2022-08-30 09:25:00 +0200

    Test commit

--- pages/kowalsky.js
+++ pages/kowalsky.js
@@ -11,7 +11,7 @@ export default function Kowalsky() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome Kowalsky!
+          Welcome Kowalsky! Test.
         </h1>
 
         <p className={styles.description}>
