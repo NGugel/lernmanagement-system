b1901aebb269a08aaa6eae31bf1bf7fac5b9eaa3
NGugel <97967066+NGugel@users.noreply.github.com>
2022-09-13 21:52:08 +0200

    fetch test 21

--- api/analysis/testtest.js
+++ api/analysis/testtest.js
@@ -2,6 +2,6 @@ export function FragenZuFolgen2() {
   let data = require('../../db.json');
   let randomNumber = Math.floor(Math.random() * data.fragenZuFolgen.length);;
   console.log(randomNumber);
-  console.log(data.fragenZuFolgen[0].question);
-  return randomNumber;
+  console.log(data.fragenZuFolgen[randomNumber].question);
+  return data.fragenZuFolgen[randomNumber].question;
 }
