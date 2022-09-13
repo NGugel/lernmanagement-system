6484702f58166ec1f59625846d8c210270e3df59
SKowalsky <36691970+SKowalsky@users.noreply.github.com>
2022-09-13 13:57:26 +0200

    Added comment of installed Plugins

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../public/assets/css/style.css'
 
+/*
+  Installed Plugins:
+    - react-boostrap
+    - @mui/material
+*/
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
