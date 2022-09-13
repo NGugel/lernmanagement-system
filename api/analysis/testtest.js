782a8c031e835e8e80a2066a2dc069d231f392e7
NGugel <97967066+NGugel@users.noreply.github.com>
2022-09-13 21:10:34 +0200

    fetch test 7

--- api/analysis/testtest.js
+++ api/analysis/testtest.js
@@ -15,7 +15,7 @@ export async function FragenZuFolgen2() {
 }*/
 
 export function FragenZuFolgen2() {
-  let data = JSON.parse('../db.json');
+  let data = JSON.parse('{"name":"John", "age":30, "city":"New York"}');
   console.log(data);
   return data;
 }
