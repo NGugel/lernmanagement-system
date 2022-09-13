c8fbf117c47be2de67bfd49feb2e69b638087398
NGugel <97967066+NGugel@users.noreply.github.com>
2022-09-13 21:56:37 +0200

    fetch test 22

--- api/analysis/testtest.js
+++ api/analysis/testtest.js
@@ -1,7 +1,15 @@
 export function FragenZuFolgen2() {
+  let data = require('../../db2.json');
+  let randomNumber = Math.floor(Math.random() * data.fragenZuFolgen.length);;
+  console.log(randomNumber);
+  console.log(data.analysis.folgen.fragenUndAntworten[randomNumber].frage);
+  return data.analysis.folgen.fragenUndAntworten[randomNumber].frage;
+}
+/*
+export function FragenZuFolgen2() {
   let data = require('../../db.json');
   let randomNumber = Math.floor(Math.random() * data.fragenZuFolgen.length);;
   console.log(randomNumber);
   console.log(data.fragenZuFolgen[randomNumber].question);
   return data.fragenZuFolgen[randomNumber].question;
-}
+}*/
