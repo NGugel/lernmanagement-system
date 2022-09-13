f409a97ec869b64ae86257eafbbde901f8c7fa8d
NGugel <97967066+NGugel@users.noreply.github.com>
2022-09-13 17:48:28 +0200

    db.json test 3

--- api/analysis/testtest.js
+++ api/analysis/testtest.js
@@ -3,18 +3,8 @@ import { useEffect, useState } from 'react';
 //import CourseCard from './CourseCard';
 
 export function FragenZuFolgen2() {
-  //const [questions, setQuestions] = useState([]);
-
-    // fetch data from github.com/ngugel/lernmanagement-system/db.json
-    // (fake REST server, see https://my-json-server.typicode.com)
-    const loadData = async () => {
-      const response = await fetch(
-        'https://my-json-server.typicode.com/ngugel/lernmanagement-system/fragenZuFolgen'
-      );
-      const data = await response.json();
-      return data;
-    };
-    //loadData().catch(console.error);
-   
-  return loadData;
+  const data = fetch(
+    'https://my-json-server.typicode.com/ngugel/lernmanagement-system/fragenZuFolgen'
+  );
+  return data;
 }
