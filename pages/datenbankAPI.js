f2fabaf9e257112c678db24a7b2b2262d6463ee4
NGugel <97967066+NGugel@users.noreply.github.com>
2022-09-13 15:59:40 +0200

    2 db.json created and datenbankAPI.js created

---
+++ pages/datenbankAPI.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import Typography from '@mui/material/Typography';
+import CourseCard from './CourseCard';
+
+export default function Courses() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    // fetch data from github.com/behrends/nextjs-nav-mui/db.json
+    // (fake REST server, see https://my-json-server.typicode.com)
+    const loadData = async () => {
+      const response = await fetch(
+        'https://my-json-server.typicode.com/ngugel/lernmanagement-system/questions'
+      );
+      const data = await response.json();
+      setQuestions(data);
+    };
+    loadData().catch(console.error);
+  }, []);
+
+  if (questions.length === 0)
+    return <h5>Keine Kurse</h5>;
+
+  return (
+    <>
+      {questions.map((question) => (
+        <p key={question.id}> {question.question}</p>
+      ))}
+    </>
+  );
+}
