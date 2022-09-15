39ed9b02d353592c6ea5eeb49e6d538bdcc389fb
NGugel <97967066+NGugel@users.noreply.github.com>
2022-09-15 20:11:22 +0200

    3 files modified, getRandomContent implementiert

--- api/datenbankAPI.js
+++ api/datenbankAPI.js
@@ -12,7 +12,7 @@ export function getAllLectures() {
   return data.vorlesungen;
 }
 
-//eine Vorlesung mittels id abrufen (Rueckgabe ist des Objekt der Vorlesung)
+//eine Vorlesung mittels ID abrufen (Rueckgabe ist des Objekt der Vorlesung)
 export function getLecture(lectureId) {
   let data = require('../db.json');
   if(data.vorlesungen[lectureId - 1] != null) {
@@ -23,7 +23,7 @@ export function getLecture(lectureId) {
 }
 
 //---------------Themen------------------
-//alle Themennamen einer Vorlesung (via ID) im Array (von Objekten) abrufen
+//alle Themen einer Vorlesung (via ID) im Array (von Objekten) abrufen
 export function getAllTopics(lectureId) {
   let data = require('../db.json');
   let allTopics = [];
@@ -38,20 +38,72 @@ export function getAllTopics(lectureId) {
   }
 }
 
-//ein Thema einer Vorlesung mittels id (1. Argument: id der Vorlesung, 2. Argument: id des Themas) abrufen (Rueckgabe ist des Objekt des Themas)
+//ein Thema einer Vorlesung mittels IDs (1. Argument: ID der Vorlesung, 2. Argument: ID des Themas) abrufen (Rueckgabe ist des Objekt des Themas)
 export function getTopic(lectureId, topicId) {
   let data = require('../db.json');
   if(data.vorlesungen[lectureId - 1] != null) {
     if(data.vorlesungen[lectureId - 1].themen[topicId - 1] != null) {
       return data.vorlesungen[lectureId - 1].themen[topicId - 1];
     } else {
-      return "Datenbankeintrag existiert, aber Themenabfrage noch nicht implementiert!"
+      return "Keine gültige ThemenID!"
     }
   } else {
     return "Keine gültige VorlesungsID!";
   }
 }
 
+//alle Fragen eines Themas einer Vorlesung (via IDs) im Array (von Objekten) abrufen
+export function getAllContent(lectureId, topicId) {
+  let data = require('../db.json');
+  if(data.vorlesungen[lectureId - 1] != null) {
+    if(data.vorlesungen[lectureId - 1].themen[topicId - 1] != null) {
+      return data.vorlesungen[lectureId - 1].themen[topicId - 1].lerninhalt;
+    } else {
+      return "Keine gültige ThemenID!"
+    }
+  } else {
+    return "Keine gültige VorlesungsID!";
+  }
+}
+
+//ein Lerninhalt eines Themas einer Vorlesung mittels IDs (1. Argument: ID der Vorlesung, 2. Argument: ID des Themas, 3. Argument: ID des Lerninhalts) abrufen (Rueckgabe ist des Objekt des Lerninhalts)
+export function getContent(lectureId, topicId, contentId) {
+  let data = require('../db.json');
+  if(data.vorlesungen[lectureId - 1] != null) {
+    if(data.vorlesungen[lectureId - 1].themen[topicId - 1] != null) {
+      if(data.vorlesungen[lectureId - 1].themen[topicId - 1].lerninhalt[contentId - 1] != null) {
+        return data.vorlesungen[lectureId - 1].themen[topicId - 1].lerninhalt[contentId - 1];
+      } else {
+        return "Keine gültige LerninhaltsID!";
+      }
+    } else {
+      return "Keine gültige ThemenID!"
+    }
+  } else {
+    return "Keine gültige VorlesungsID!";
+  }
+}
+
+//eine zufaelliger Lerninhalt eines Themas einer Vorlesung mittels IDs (1. Argument: ID der Vorlesung, 2. Argument: ID des Themas, 3. Argument: ID des Lerninhalts) abrufen (Rueckgabe ist des Objekt des Lerninhalts)
+export function getRandomContent(lectureId, topicId) {
+  let data = require('../db.json');
+  if(data.vorlesungen[lectureId - 1] != null) {
+    if(data.vorlesungen[lectureId - 1].themen[topicId - 1] != null) {
+      if(data.vorlesungen[lectureId - 1].themen[topicId - 1].lerninhalt[contentId - 1] != null) {
+        let randomNumber = Math.floor(Math.random() * data.vorlesungen[lectureId - 1].themen[topicId - 1].lerninhalt.length);
+        return data.vorlesungen[lectureId - 1].themen[topicId - 1].lerninhalt[randomNumber];
+      } else {
+        return "Keine gültige LerninhaltsID!";
+      }
+    } else {
+      return "Keine gültige ThemenID!"
+    }
+  } else {
+    return "Keine gültige VorlesungsID!";
+  }
+}
+/*
+Deprecated > spaeter loeschen
 //---------------Analysis------------------
 //Folgen
 export function getRandomFolgenQuestion() {
@@ -103,3 +155,4 @@ export function getRandomAlgorithmenQuestion() {
   let randomNumber = Math.floor(Math.random() * data.theoretischeInformatikI.algorithmen.fragenUndAntworten.length);
   return data.theoretischeInformatikI.algorithmen.fragenUndAntworten[randomNumber].frage;
 }
+*/
