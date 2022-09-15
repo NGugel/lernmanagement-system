a83c2919e78fbbcb8e56d88d7fbb56554e1e39bb
NGugel <97967066+NGugel@users.noreply.github.com>
2022-09-15 16:22:24 +0200

    1 file modified, API Aufruf Korrektur

--- pages/analysis-reihen.js
+++ pages/analysis-reihen.js
@@ -32,7 +32,7 @@ export default function Reihen() {
           <Col lg="4">
             <img className="roundedImage" src="assets/image/Mathejpg.jpg"/>
 
-            <h2 className="fw-normal">Analysis {API.getAllLectures}</h2>
+            <h2 className="fw-normal">Analysis {API.getAllLectures()}</h2>
             <p>Hier lernen Sie alles rund um Folgen, Reihen und Abbildungen.</p>
           </Col>
 
