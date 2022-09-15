ea1daedd876bda29af047e5e9d82320cdf199f26
NGugel <97967066+NGugel@users.noreply.github.com>
2022-09-15 16:34:43 +0200

    1 file modified, getLecture test 2

--- pages/analysis-reihen.js
+++ pages/analysis-reihen.js
@@ -32,7 +32,7 @@ export default function Reihen() {
           <Col lg="4">
             <img className="roundedImage" src="assets/image/Mathejpg.jpg"/>
 
-            <h2 className="fw-normal">Analysis {API.getLecture(2)}</h2>
+            <h2 className="fw-normal">Analysis {API.getLecture(3)}</h2>
             <p>Hier lernen Sie alles rund um Folgen, Reihen und Abbildungen.</p>
           </Col>
 
